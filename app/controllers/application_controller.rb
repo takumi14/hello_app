95b8b86ca99366dd18f556f8302b4d3f6ef60fa2
takumi14 <03serious@gmail.com>
2019-07-06 12:48:13 +0900

    rails-new-hello_app

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+
+
+  def hello
+    render html:"hola, mundo!"
+  end
+
+
+
+  def goodbye
+    render html:"goodbye, world!"
+  end
+
+end
